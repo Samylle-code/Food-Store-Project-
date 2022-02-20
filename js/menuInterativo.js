56b0870c7a8ba6885ea8246a49d3fca37ec5a61b
Samylle <samyllecaetano@hotmail.com>
2022-02-20 10:19:44 -0300

    menu demo

--- js/menuInterativo.js
+++ js/menuInterativo.js
@@ -1,13 +1,13 @@
-const $menu = document.querySelector('.header__nav')
-const $btnMenuOpen = document.querySelector('.btnMenu btnMenu_open')
-const $btnMenuClose = document.querySelector('.btnMenu btnMenu_close')
+const $menu = document.querySelector('.header__nav.active');
+const $btnMenuOpen = document.querySelector('.btnMenu btnMenu_open');
+const $btnMenuClose = document.querySelector('.btnMenu btnMenu_close');
 
 $btnMenuOpen.addEventListener('click', function() {
-  $menu.classList.add('header__nav')
-  $menu.classList.add('header__nav.active')
+  $menu.classList.add('.header__nav.active')
+
 })
 
 $btnMenuClose.addEventListener('click', function() {
-  $menu.classList.remove('header__nav')
-  $menu.classList.remove('header__nav.active')
+  $menu.classList.remove('.header__nav.active')
+
 })
